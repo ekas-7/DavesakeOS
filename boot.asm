fb063f161318e9d589c6c85e14eec013760490f8
ekas-7 <ekaspreetatwal@gmail.com>
2025-02-16 07:14:35 +0530

    file 2

--- boot.asm
+++ boot.asm
@@ -1,4 +1,23 @@
+mov bx, 5
+cmp bx, 5
+je label
 jmp $
+
+label:
+        mov ah, 0x0e
+        mov al, 'A'
+        int 0x10
+
+
+loop:
+        inc al 
+        cmp al, 'Z'+1
+        je exit
+        int 0x10
+        jmp loop
+
+exit:
+        jmp $
 
 times 510-($-$$) db 0
 db 0x55 , 0xaa
